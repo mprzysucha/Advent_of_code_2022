943111d2c3eb8d5360dea821ef7ced1c7e641d70
Michal Przysucha <mprzysucha@plume.com>
2022-12-29 11:28:49 +0100

    use lazy static + small refactor

--- day_25/src/main.rs
+++ day_25/src/main.rs
@@ -1,5 +1,10 @@
+// #[macro_use]
+// extern crate lazy_static;
+
 use std::cmp::max;
 use aoctools::read_whole_file;
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 
 fn main() {
     let content = read_whole_file("input.txt");
@@ -74,6 +79,24 @@ fn snafu_to_dec(s: String) -> (i128, usize) {
 }
 
 fn dec_to_snafu(d: i128, only_twos_in_dec: &Vec<i128>) -> String {
+
+    fn convert_remaining(remain: i128, exp: usize, only_twos_in_dec: &Vec<i128>) -> String {
+        let mut res = String::from("");
+        let minus_remain_in_snafu = dec_to_snafu(remain.abs(), only_twos_in_dec);
+        let num_of_chars = minus_remain_in_snafu.chars().into_iter().collect::<Vec<char>>().len();
+        for _ in 0..exp - num_of_chars {
+            res.push('0');
+        }
+        for c in minus_remain_in_snafu.chars() {
+            if remain > 0 {
+                res.push(c);
+            } else {
+                res.push(*NEGATIVE_CHARS.get(&c).unwrap());
+            }
+        }
+        res
+    }
+
     if d >= -2 && d <= 2 {
         if d == -2 {
             return "=".to_string();
@@ -97,26 +120,7 @@ fn dec_to_snafu(d: i128, only_twos_in_dec: &Vec<i128>) -> String {
 
     if d > POWERS_OF_5[exp] + only_twos_in_dec[exp - 1] { // d > 1222, should return 2_____ with negative in underscore (need to subtract from 20000)
         let mut res = "2".to_string();
-        let remain = d - 2 * POWERS_OF_5[exp]; // negative number
-        if remain <= 0 {
-            let minus_remain_in_snafu = dec_to_snafu(-remain, only_twos_in_dec);
-            let num_of_chars = minus_remain_in_snafu.chars().into_iter().collect::<Vec<char>>().len();
-            for _ in 0..exp - num_of_chars {
-                res.push('0');
-            }
-            for c in minus_remain_in_snafu.chars() {
-                res.push(negative(c));
-            }
-        } else {
-            let minus_remain_in_snafu = dec_to_snafu(remain, only_twos_in_dec);
-            let num_of_chars = minus_remain_in_snafu.chars().into_iter().collect::<Vec<char>>().len();
-            for _ in 0..exp - num_of_chars {
-                res.push('0');
-            }
-            for c in minus_remain_in_snafu.chars() {
-                res.push(c);
-            }
-        }
+        res.push_str(convert_remaining(d - 2 * POWERS_OF_5[exp], exp, &only_twos_in_dec).as_str());
         res
 
     } else if d == POWERS_OF_5[exp] + only_twos_in_dec[exp - 1] { // d == 1222
@@ -127,48 +131,40 @@ fn dec_to_snafu(d: i128, only_twos_in_dec: &Vec<i128>) -> String {
         res
     } else { // 222 < d < 1222, should return 1____
         let mut res = "1".to_string();
-        let remain = d - POWERS_OF_5[exp];
-        if remain <= 0 {  // should return 1____ with negative in underscore (need to subtract from 10000)
-            let minus_remain_in_snafu = dec_to_snafu(-remain, only_twos_in_dec);
-            let num_of_chars = minus_remain_in_snafu.chars().into_iter().collect::<Vec<char>>().len();
-            for _ in 0..exp - num_of_chars {
-                res.push('0');
-            }
-            for c in minus_remain_in_snafu.chars() {
-                res.push(negative(c));
-            }
-            res
-        } else { // should return 1____ with positive in underscore (need to add to 10000)
-            let minus_remain_in_snafu = dec_to_snafu(remain, only_twos_in_dec);
-            let num_of_chars = minus_remain_in_snafu.chars().into_iter().collect::<Vec<char>>().len();
-            for _ in 0..exp - num_of_chars {
-                res.push('0');
-            }
-            for c in minus_remain_in_snafu.chars() {
-                res.push(c);
-            }
-            res
-        }
-
+        res.push_str(convert_remaining(d - POWERS_OF_5[exp], exp, &only_twos_in_dec).as_str());
+        res
     }
 }
 
 
-fn negative(c: char) -> char {
-    if c == '2' {
-        return '=';
-    } else if c == '1' {
-        return '-';
-    } else if c == '0' {
-        return '0';
-    } else if c == '-' {
-        return '1';
-    } else if c == '=' {
-        return '2';
-    } else {
-        panic!("Not know negative for {}", c);
-    }
-}
+// fn negative(c: char) -> char {
+//     if c == '2' {
+//         return '=';
+//     } else if c == '1' {
+//         return '-';
+//     } else if c == '0' {
+//         return '0';
+//     } else if c == '-' {
+//         return '1';
+//     } else if c == '=' {
+//         return '2';
+//     } else {
+//         panic!("Not know negative for {}", c);
+//     }
+// }
 
 const POWERS_OF_5: [i128; 51] = [1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625, 1220703125, 6103515625, 30517578125, 152587890625, 762939453125, 3814697265625, 19073486328125, 95367431640625, 476837158203125, 2384185791015625, 11920928955078125, 59604644775390625, 298023223876953125, 1490116119384765625, 7450580596923828125, 37252902984619140625, 186264514923095703125, 931322574615478515625, 4656612873077392578125, 23283064365386962890625, 116415321826934814453125, 582076609134674072265625, 2910383045673370361328125, 14551915228366851806640625, 72759576141834259033203125, 363797880709171295166015625, 1818989403545856475830078125, 9094947017729282379150390625, 45474735088646411895751953125, 227373675443232059478759765625, 1136868377216160297393798828125, 5684341886080801486968994140625, 28421709430404007434844970703125, 142108547152020037174224853515625, 710542735760100185871124267578125, 3552713678800500929355621337890625, 17763568394002504646778106689453125, 88817841970012523233890533447265625];
 const SNAFU_CHARS: [char; 5] = ['=', '-', '0', '1', '2'];
+
+
+lazy_static! {
+    static ref NEGATIVE_CHARS: HashMap<char, char> = {
+        let mut map = HashMap::new();
+        map.insert('2', '=');
+        map.insert('1', '-');
+        map.insert('0', '0');
+        map.insert('-', '1');
+        map.insert('=', '2');
+        map
+    };
+}
